8489224d5ff58d583bfef8690a17207145958601
UXGitHub <uxdeploy@gmail.com>
2015-04-18 18:37:25 -0300

    layout login screen

--- index.php
+++ index.php
@@ -1,58 +1,20 @@
 <!DOCTYPE html>
 <html lang="en" ng-app="myApp">
-
   <head>
     <meta charset="utf-8">
-      <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width,initial-scale=1">
-          <title>AppMenu</title>
-          <!-- Bootstrap -->
-          <link href="css/bootstrap.min.css" rel="stylesheet">
-            <link href="css/custom.css" rel="stylesheet">
-              <link href="css/toaster.css" rel="stylesheet">
-                <style>
-                  a {
-                  color: orange;
-                  }
-                </style>
-                <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
-                <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
-                <!--[if lt IE 9]><link href= "css/bootstrap-theme.css"rel= "stylesheet" >
-
-<script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
-<script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
-<![endif]-->
-              </head>
-
-  <body ng-cloak="">
-<!--     <div class="navbar navbar-inverse navbar-fixed-top" role="navigation">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width,initial-scale=1">
+    <title>AppMenu</title>
+    <link href="css/style.css" rel="stylesheet">
+    <link href="css/toaster.css" rel="stylesheet">
+  </head>
+  <body>
+  <div id="background"></div>
+    <div>
       <div class="container">
-        <div class="row">
-          <div class="navbar-header col-md-8">
-            <button type="button" class="navbar-toggle" toggle="collapse" target=".navbar-ex1-collapse">
-              <span class="sr-only">Toggle navigation</span>
-              <span class="icon-bar"></span>
-              <span class="icon-bar"></span>
-              <span class="icon-bar"></span>
-            </button>
-            <a class="navbar-brand" rel="home" title="AngularJS Authentication App">AngularJS Authentication App</a>
-          </div>
-          <div class="navbar-header col-md-2">
-            <a class="navbar-brand" rel="home" title="AngularJS Authentication Tutorial" href="http://www.angularcode.com/user-authentication-using-angularjs-php-mysql">Tutorial</a>
-          </div>
-           <div class="navbar-header col-md-2">
-            <a class="navbar-brand" rel="home" title="Download" href="https://app.box.com/s/1uvn9xo9nbi4xxm9g9dx">Download</a>
-          </div>
-        </div>
+        <div data-ng-view="" id="ng-view"></div>
       </div>
-    </div> -->
-    <div >
-      <div class="container">
-
-        <div data-ng-view="" id="ng-view" class="slide-animation"></div>
-
-      </div>
-    </body>
+  </body>
   <toaster-container toaster-options="{'time-out': 3000}"></toaster-container>
   <!-- Libs -->
   <script src="js/angular.min.js"></script>
@@ -62,6 +24,7 @@
   <script src="app/app.js"></script>
   <script src="app/data.js"></script>
   <script src="app/directives.js"></script>
+  <!-- CONTROLLERS -->
+  <script src="app/changePasswordCtrl.js"></script>
   <script src="app/authCtrl.js"></script>
 </html>
-
